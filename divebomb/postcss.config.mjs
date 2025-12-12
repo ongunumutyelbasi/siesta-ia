13aed4fe6746c7cf195edad7c751b7935a799ea4
ongunumutyelbasi <umutyelbasi@gmail.com>
2025-12-12 12:50:59 +0000

    clean: Remove all external project files and virtual environments

--- divebomb/postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-// postcss.config.mjs (Corrected)
-export default {
-  plugins: {
-    // Correctly reference the installed package: @tailwindcss/postcss
-    "@tailwindcss/postcss": {}, 
-    autoprefixer: {},
-  },
-};
